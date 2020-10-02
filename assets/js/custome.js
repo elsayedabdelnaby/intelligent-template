b71559b2354d40fac279918b3a666760d508941d
elaraby <elsayed.fcis.1@gmail.com>
2020-10-03 00:31:39 +0200

    handel submenu at navbar

--- assets/js/custome.js
+++ assets/js/custome.js
@@ -1,15 +1,19 @@
 $(document).ready(function(){
-	$(".dropdown").hover(function(){
-		var dropdownMenu = $(this).children(".dropdown-menu");
-		if(dropdownMenu.is(":visible")){
-			dropdownMenu.parent().toggleClass("open");
-		}
-	});
-
 	$('#global-search .input-group').hover(function(){
 		$('#search-modules-list').toggle("show");
 	});
 	$('#search-modules-list li').click(function(){
 		$('#search-option').html($(this).html());
+	});
+
+	$('.getMenu').on('mouseover', function(){
+		$('.submenu').hide();
+		$("." + $(this).data('type') + "-submenu").show();
+	});
+	$('.submenu').on('mouseover', function(){
+		$(this).show();
+	});
+	$('.getMenu, .submenu').on('mouseleave', function(){
+		$('.submenu').hide();
 	});
 });		
